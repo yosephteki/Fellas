a76ac228831a77b2f4c855faea67f2d69b753cbc
Yosep Teki <yosephteki@gmail.com>
2021-04-27 15:15:58 +0700

    change sslmode for heroku

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	var err error
 	fmt.Println("HELLO WORLD!")
 
-	Config.DB, err = gorm.Open("postgres", "host=ec2-54-166-167-192.compute-1.amazonaws.com port=5432 user=nrojvokwxfufac dbname=d1ihg08luvjgbb sslmode=require password=25bdb986f6d7469052ab3468b88b900886a70a510aaf19d5b580b65bba36b624")
+	Config.DB, err = gorm.Open("postgres", "host=ec2-54-166-167-192.compute-1.amazonaws.com port=5432 user=nrojvokwxfufac dbname=d1ihg08luvjgbb sslmode=disable password=25bdb986f6d7469052ab3468b88b900886a70a510aaf19d5b580b65bba36b624")
 	if err != nil {
 		fmt.Println("Status : ", err)
 	}
