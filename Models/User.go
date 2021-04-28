5ba3dbf3d17644712d95ae13d8fa08d2072d864c
Yosep Teki <yosephteki@gmail.com>
2021-04-28 12:02:20 +0700

    change user model field id to []uint8

--- Models/User.go
+++ Models/User.go
@@ -7,12 +7,12 @@ import (
 )
 
 type User struct {
-	Id       uint   `json:"id"`
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Phone    string `json:"phone"`
-	Address  string `json:"address"`
-	Password string `json:"password"`
+	Id       []uint8 `json:"id"`
+	Name     string  `json:"name"`
+	Email    string  `json:"email"`
+	Phone    string  `json:"phone"`
+	Address  string  `json:"address"`
+	Password string  `json:"password"`
 }
 
 func (u *User) TableName() string {
